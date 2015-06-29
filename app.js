48a78aa8df2811dd95f440bcb101c3022608f7a8
Mike Lague <mike@lague.org>
2015-06-29 17:04:25 -0400

    add top answerers functionality

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ $(document).ready( function() {
 		var tags = $(this).find("input[name='tags']").val();
 		getUnanswered(tags);
 	});
+	$('.inspiration-getter').submit( function(event){
+		// zero out results if previous search has run
+		$('.results').html('');
+		// get the value of the tag the user submitted
+		var tag = $(this).find("input[name='answerers']").val();
+		getInspiration(tag, "all_time");
+	});
 });
 
 // this function takes the question object returned by StackOverflow 
@@ -41,6 +48,33 @@ var showQuestion = function(question) {
 	return result;
 };
 
+// this function takes tag score object returned by StackOverflow 
+// and creates new result to be appended to DOM
+var showAnswerer = function(answerer) {
+	
+	// clone our result template code
+	var result = $('.templates .answerer').clone();
+	
+	// Set the name of the user (answerer)
+	var nameElem = result.find('.name-text a');
+	nameElem.attr('href', answerer.user.link);
+	nameElem.text(answerer.user.display_name);
+
+	// set the reputation of the user
+	var reputation = result.find('.reputation');
+	reputation.text(answerer.user.reputation);
+
+	// set the post count
+	var post_count = result.find('.post-count');
+	post_count.text(answerer.post_count);
+
+	// set the score
+	var score = result.find('.score');
+	score.text(answerer.score);
+	
+	return result;
+};
+
 
 // this function takes the results object from StackOverflow
 // and creates info about search results to be appended to DOM
@@ -55,6 +89,7 @@ var showError = function(error){
 	var errorText = '<p>' + error + '</p>';
 	errorElem.append(errorText);
 };
+
 
 // takes a string of semi-colon separated tags to be searched
 // for on StackOverflow
@@ -87,6 +122,41 @@ var getUnanswered = function(tags) {
 		$('.search-results').append(errorElem);
 	});
 };
+
+// takes a tag to be searched for on StackOverflow
+var getInspiration = function(tag, period) {
+	
+	// the parameters we need to pass in our request to StackOverflow's API
+	var url = "http://api.stackexchange.com/2.2/tags/"
+				+ tag + "/top-answerers/" + period ;
+	var request = {site: 'stackoverflow'};
+		
+	var result = $.ajax({
+		url: url,
+		data: request,
+		dataType: "jsonp",
+		type: "GET",
+		})
+	.done(function(result){
+		var searchResults = showSearchResults(tag, result.items.length);
+
+		$('.search-results').html(searchResults);
+
+		console.log(result.items);
+
+		$.each(result.items, function(i, item) {
+			var question = showAnswerer(item);
+			$('.results').append(question);
+		});
+	})
+	.fail(function(jqXHR, error, errorThrown){
+		var errorElem = showError(error);
+		$('.search-results').append(errorElem);
+	});
+};
+
+
+
 
 
 
